3b09ab239237cedd3604fc1590d2285faa2ae344
jnu3d1 <jnu3d1@rambler.ru>
2022-07-30 17:41:17 +0600

    Этап 4 Создана модель Answer Изменения, которые включены в коммит: 	новый файл:    webapp/migrations/0002_answer_poll_choices_alter_choice_poll_answer_choice_and_more.py 	изменено:      webapp/models.py

--- webapp/models.py
+++ webapp/models.py
@@ -6,6 +6,8 @@ from django.db import models
 class Poll(models.Model):
     question = models.CharField(max_length=50, verbose_name='Вопрос')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания опроса')
+    choices = models.ManyToManyField('webapp.Choice', related_name='polls', through='webapp.Answer',
+                                     through_fields=('poll', 'choice'))
 
     def __str__(self):
         return self.question
@@ -18,7 +20,7 @@ class Poll(models.Model):
 
 class Choice(models.Model):
     text = models.CharField(max_length=50, verbose_name='Вариант ответа')
-    poll = models.ForeignKey('webapp.Poll', on_delete=models.CASCADE, related_name='choices', verbose_name='Опрос')
+    poll = models.ForeignKey('webapp.Poll', on_delete=models.CASCADE, related_name='ch0ices', verbose_name='Опрос')
 
     def __str__(self):
         return self.text
@@ -27,3 +29,10 @@ class Choice(models.Model):
         db_table = 'choices'
         verbose_name = 'Вариант ответа'
         verbose_name_plural = 'Варианты ответа'
+
+
+class Answer(models.Model):
+    poll = models.ForeignKey('webapp.Poll', on_delete=models.CASCADE, related_name='ch0ice', verbose_name='Опрос')
+    choice = models.ForeignKey('webapp.Choice', on_delete=models.CASCADE, related_name='p0ll',
+                               verbose_name='Вариант ответа')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата прохождения опроса')
