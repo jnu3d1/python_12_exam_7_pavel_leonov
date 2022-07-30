6b31d84bfb72b85ee9d511843463916e76eae875
jnu3d1 <jnu3d1@rambler.ru>
2022-07-30 14:30:18 +0600

    Этап 2 Реализована возможность детального просмотра опросов Изменения, которые включены в коммит: 	изменено:      webapp/templates/polls/index.html 	новый файл:    webapp/templates/polls/poll.html 	изменено:      webapp/urls.py 	изменено:      webapp/views/polls_views.py

--- webapp/urls.py
+++ webapp/urls.py
@@ -4,4 +4,5 @@ from webapp.views import *
 
 urlpatterns = [
     path('', PollsView.as_view(), name='index'),
+    path('poll/<int:pk>/', PollView.as_view(), name='poll'),
 ]
--- webapp/views/polls_views.py
+++ webapp/views/polls_views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from webapp.models import Poll
 
@@ -12,3 +12,13 @@ class PollsView(ListView):
     context_object_name = 'polls'
     ordering = ('-created_at')
     paginate_by = 5
+
+
+class PollView(DetailView):
+    model = Poll
+    template_name = 'polls/poll.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['choises'] = self.object.choices.all()
+        return context
