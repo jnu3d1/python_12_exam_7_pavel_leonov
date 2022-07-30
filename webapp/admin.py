9ef00e70cd0500e4cdf85e72ff014fb56588a77d
jnu3d1 <jnu3d1@rambler.ru>
2022-07-30 12:29:31 +0600

    Этап 1 Созданы модели Poll и Choice Модели зарегистрированы в админ-панели Создана фикстура тестовых данных Изменения, которые включены в коммит: 	новый файл:    fixtures/dump.json 	изменено:      webapp/admin.py 	новый файл:    webapp/migrations/0001_initial.py 	изменено:      webapp/models.py

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from webapp.models import Poll, Choice
+
+
+class PollsAdmin(admin.ModelAdmin):
+    list_display = ['question']
+
+
+admin.site.register(Poll, PollsAdmin)
+
+
+class ChoicesAdmin(admin.ModelAdmin):
+    list_display = ['text']
+
+
+admin.site.register(Choice, ChoicesAdmin)
--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Poll(models.Model):
+    question = models.CharField(max_length=50, verbose_name='Вопрос')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания опроса')
+
+    def __str__(self):
+        return self.question
+
+    class Meta:
+        db_table = 'polls'
+        verbose_name = 'Опрос'
+        verbose_name_plural = 'Опросы'
+
+
+class Choice(models.Model):
+    text = models.CharField(max_length=50, verbose_name='Вариант ответа')
+    poll = models.ForeignKey('webapp.Poll', on_delete=models.CASCADE, related_name='choices', verbose_name='Опрос')
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        db_table = 'choices'
+        verbose_name = 'Вариант ответа'
+        verbose_name_plural = 'Варианты ответа'
